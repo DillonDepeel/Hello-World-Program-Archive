351dd104813ced3bf1647773e3697c22346286fd
Dillon Depeel <dillondepeel@hotmail.com>
2022-06-20 16:53:50 -0600

    Update C# Hello World.cs

--- C# Hello World.cs
+++ C# Hello World.cs
@@ -1,1 +1,14 @@
-Console.WriteLine("Hello World!");
+using System;
+
+namespace HelloWorld
+{
+     class Program
+     {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Hello World!");
+             Console.WriteLine("Press Enter Key to Exit.");
+             Console.ReadLine();
+        }
+     }
+}
